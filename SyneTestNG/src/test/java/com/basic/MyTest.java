5ef810adc91d14f19b1bd55b95dfa690062cc1a0
Anil.Kale <Anil.Kale@SYN-1DM35BS.synechron.com>
2018-08-03 14:34:30 +0530

    TestNG with maven code

---
+++ SyneTestNG/src/test/java/com/basic/MyTest.java
@@ -0,0 +1,20 @@
+package com.basic;
+
+import org.testng.annotations.Test;
+
+public class MyTest {
+	
+	@Test
+	public void test1(){
+		System.out.println(" test 1");
+	}
+	@Test
+	public void test2() {
+		System.out.println(" test 2");
+	}
+	@Test
+	public void test3() {
+		System.out.println(" test 3");
+	}
+
+}
